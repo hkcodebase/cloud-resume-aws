fbd27d653af2d3c2b5436cf907942defb78b4b29
hkcodebase <hkchauhan022@gmail.com>
2024-04-28 11:51:32 -0400

    counter url from aws

--- hk-cloud-resume/src/components/Counter.tsx
+++ hk-cloud-resume/src/components/Counter.tsx
@@ -5,17 +5,10 @@ export default function Counter() {
     const [count, setCount] = useState(0);
 
     useEffect(() => {
-        fetch('http://localhost:8080/counter',{method:"GET"})
+        fetch('https://backend.hkcloudresume.com/counter',{method:"GET"})
             .then(response => response.json())
             .then(data => setCount(data));
     }, []); // Empty array means this effect runs once on mount
-
-
-         //fetch('https://backend.hkcloudresume.com/counter')
-
-
-
-
     return (
     <div>
       <h5>You are visitor number : {count}</h5>
