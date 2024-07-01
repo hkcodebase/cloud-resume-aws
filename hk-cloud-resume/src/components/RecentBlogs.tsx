94a746a265d9550d5eb74d451b41311edb5ee9b0
hkcodebase <hkchauhan022@gmail.com>
2024-07-01 17:04:22 -0400

    Recent blogs Updated

--- hk-cloud-resume/src/components/RecentBlogs.tsx
+++ hk-cloud-resume/src/components/RecentBlogs.tsx
@@ -6,29 +6,28 @@ export default function RecentBlogs(){
             <h4>Recent Blogs at Medium</h4>
             <List>
                 <ListInlineItem>
-                    <a href="https://medium.com/@hkcodeblogs/deploy-a-spring-boot-application-to-aws-ecs-91742a32a5f1" target={"_blank"}>
-                        Deploy Spring boot application to AWS ECS</a>
-                </ListInlineItem>
-                <ListInlineItem>
-                    <a href="https://hkcodeblogs.medium.com/cloud-resume-using-aws-services-part-1-69e000894f73" target={"_blank"}>
-                        Hosting Cloud Resume over AWS</a>
+                    <a href="https://hkcodeblogs.medium.com/rest-apis-using-springboot-3-userbook-application-43df796f69fd" target={"_blank"}>
+                        Develop REST Based Microservice using Springboot
+                    </a>
                 </ListInlineItem>
                 <ListInlineItem>
                     <a href="https://hkcodeblogs.medium.com/create-docker-image-and-run-spring-boot-3-based-microservice-f57258f148b" target={"_blank"}>
-                        Dockerizing Spring boot App</a>
-                </ListInlineItem>
-                <ListInlineItem>
-                    <a href="https://hkcodeblogs.medium.com/rest-apis-using-springboot-3-userbook-application-43df796f69fd" target={"_blank"}>
-                        Complete Microservice using Springboot
-                    </a>
+                        Dockerize Spring boot Microservice</a>
                 </ListInlineItem>
                 <ListInlineItem>
                     <a href="https://hkcodeblogs.medium.com/deploy-springboot-3-based-microservice-on-aws-ec2-4b9efdebb798" target={"_blank"}>
                         Deploy Springboot and maven based microservice in AWS EC2 Instance
                     </a>
                 </ListInlineItem>
+                <ListInlineItem>
+                    <a href="https://medium.com/@hkcodeblogs/deploy-a-spring-boot-application-to-aws-ecs-91742a32a5f1" target={"_blank"}>
+                        Deploy Spring boot application to AWS ECS</a>
+                </ListInlineItem>
+                <ListInlineItem>
+                    <a href="https://hkcodeblogs.medium.com/cloud-resume-using-aws-services-part-1-69e000894f73" target={"_blank"}>
+                        Cloud Resume over AWS</a>
+                </ListInlineItem>
             </List>
-
         </>
     )
 }
